e319f5fd2db2581c72e2ec10bcd65258392aac87
james-aung <james.aung@c-openai.com>
2023-12-15 15:53:00 -0500

    Add ActorNetwork class to PPO module

--- ppo.py
+++ ppo.py
@@ -42,3 +42,33 @@ class PPOMemory:
         self.rewards = []
         self.dones = []
 
+class ActorNetwork(nn.Module): # The nn.Module class is the base class for all neural network modules in PyTorch
+    def __init__(self, n_actions, input_dims, alpha, fc1_dims=256, fc2_dims=256, chkpt_dir='tmp/ppo'): # checkpoint directory is where we will save the model
+        super(ActorNetwork, self).__init__()
+
+        self.checkpoint_file = os.path.join(chkpt_dir, 'actor_torch_ppo')
+        self.actor = nn.Sequential(
+            nn.Linear(*input_dims, fc1_dims), # TODO Why do we unpack input_dims? How many dimensions does input_dims have?
+            nn.ReLU(),
+            nn.Linear(fc1_dims, fc2_dims),
+            nn.ReLU(),
+            nn.Linear(fc2_dims, n_actions),
+            nn.Softmax(dim=-1) #dim=-1 means we are applying softmax across the action dimension and not the batch dimension
+        )
+
+        self.optimizer = optim.Adam(self.parameters(), lr=alpha)
+        self.device = T.device('cuda:0' if T.cuda.is_available() else 'cpu')
+        self.to(self.device)
+
+    def forward(self, state):
+        distribution = self.actor(state)
+        distribution = Categorical(distribution)
+        return distribution
+    
+    def save_checkpoint(self):
+        # save the model parameters
+        T.save(self.state_dict(), self.checkpoint_file) # state_dict() returns a dictionary containing a whole state of the module
+
+    def load_checkpoint(self):
+        self.load_state_dict(T.load(self.checkpoint_file))
+
