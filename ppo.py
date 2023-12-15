8b5f153ea6c47a0b1a021c5e7387a1eb5a78581d
james-aung <james.aung@c-openai.com>
2023-12-15 16:00:19 -0500

    Add CriticNetwork class to ppo.py

--- ppo.py
+++ ppo.py
@@ -72,3 +72,30 @@ class ActorNetwork(nn.Module): # The nn.Module class is the base class for all n
     def load_checkpoint(self):
         self.load_state_dict(T.load(self.checkpoint_file))
 
+class CriticNetwork(nn.Module):
+    def __init__(self, inputs_dims, alpha, fc1_dims=256, fc2_dims=256, chkpt_dir='tmp/ppo'):
+        super(CriticNetwork, self).__init__()
+
+        self.checkpoint_file = os.path.join(chkpt_dir, 'critic_torch_ppo')
+        self.critic = nn.Sequential(
+            nn.Linear(*inputs_dims, fc1_dims),
+            nn.ReLU(),
+            nn.Linear(fc1_dims, fc2_dims),
+            nn.ReLU(),
+            nn.Linear(fc2_dims, 1) # output is 1 because Critic predicts the state value
+        )
+
+        self.optimizer = optim.Adam(self.parameters(), lr=alpha)
+        self.device = T.device('cuda:0' if T.cuda.is_available() else 'cpu')
+        self.to(self.device)
+
+    def forward(self, state)
+        value = self.critic(state)
+        return value
+    
+    def save_checkpoint(self):
+        T.save(self.state_dict(), self.checkpoint_file)
+
+    def load_checkpoint(self):
+        self.load_state_dict(T.load(self.checkpoint_file))
+
